7b78272b0977ca065d3559284b82eb57752645ff
Filipe Gomes <fgdl.py91@gmail.com>
2021-02-10 10:50:34 -0300

    Adicionado TDD e Baby Steps close #20

--- libpythonpro_fgomes/tests/test_spam/test_enviador_de_email.py
+++ libpythonpro_fgomes/tests/test_spam/test_enviador_de_email.py
@@ -4,3 +4,13 @@ from libpythonpro_fgomes.spam.enviador_de_email import Enviador
 def test_criar_enviador_de_email():
     enviador = Enviador()
     assert enviador is not None
+
+def teste_remetente():
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        'fgdl.py91@gmail.com',
+        'filipegomesdev@gmail.com',
+        'Curso Python Pro',
+        'Aula sobre TDD e Baby Steps.'
+    )
+    assert 'fgdl.py91@gmail.com' in resultado
