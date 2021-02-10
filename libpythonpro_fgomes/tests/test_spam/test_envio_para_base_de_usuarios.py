69a211a0cb24edbbeadc525455b808b1f45f6e33
Filipe Gomes <fgdl.py91@gmail.com>
2021-02-10 17:45:20 -0300

    Inseção de dependências close #34

--- libpythonpro_fgomes/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro_fgomes/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,10 +1,59 @@
+import pytest
+
 from libpythonpro_fgomes.spam.enviador_de_email import Enviador
 from libpythonpro_fgomes.spam.main import EnviadorDeSpam
+from libpythonpro_fgomes.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Filipe', email='fgdl.py91@gmail.com'),
+            Usuario(nome='Luffy', email='luffy_test@email.com')
+        ],
+        [
+            Usuario(nome='Filipe', email='fgdl.py91@gmail.com'),
+        ]
+    ]
+)
+def test_qde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.eviar_emails(
+        'fgdl.py91@gmail.com',
+        'Curso Python Pro',
+        'Aula arquivo Conftest.'
+    )
+    assert len(usuarios) == enviador.qtd_email_enviados
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Filipe', email='fgdl.py91@gmail.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.eviar_emails(
+        'luffy_test91@email.com',
+        'Curso Python Pro',
+        'Aula arquivo Conftest.'
+    )
+    assert enviador.parametros_de_envio == (
+        'luffy_test91@email.com',
         'fgdl.py91@gmail.com',
         'Curso Python Pro',
         'Aula arquivo Conftest.'
--- libpythonpro_fgomes/tests/test_spam/test_usuarios.py
+++ libpythonpro_fgomes/tests/test_spam/test_usuarios.py
@@ -2,13 +2,15 @@ from libpythonpro_fgomes.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Filipe')
+    usuario = Usuario(nome='Filipe', email='fgdl.py91@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Filipe'), Usuario(nome='Luffy')]
+    usuarios = [
+        Usuario(nome='Filipe', email='fgdl.py91@gmail.com'), Usuario(nome='Luffy', email='luffy_test@email.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
